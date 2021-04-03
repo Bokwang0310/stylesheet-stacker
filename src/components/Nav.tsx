b0427bda27ffa1a41c593ec1fe38b835aafca701
bokwang <chdl2353@naver.com>
2021-04-03 22:47:54 +0900

    Solve: nav indicator error

--- src/components/Nav.tsx
+++ src/components/Nav.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import { useState, useEffect } from 'react';
 import { Link, useHistory } from 'react-router-dom';
 
 import Tabs from '@material-ui/core/Tabs';
@@ -7,9 +7,21 @@ import Tab from '@material-ui/core/Tab';
 function Nav() {
   const history = useHistory();
   const tabs = ['/sheets', '/setting', '/sheets/', '/setting/'];
-  const currentPathIndex = tabs.findIndex(
-    tabPath => history.location.pathname === tabPath
-  );
+
+  const [pathName, setPath] = useState(history.location.pathname);
+
+  useEffect(() => {
+    const unlisten = history.listen((location, _) => {
+      setPath(location.pathname);
+    });
+
+    return () => {
+      unlisten();
+    };
+  });
+
+  const currentPathIndex = tabs.findIndex(tabPath => pathName === tabPath);
+
   return (
     <Tabs
       value={currentPathIndex === 0 || currentPathIndex === 2 ? 0 : 1}
