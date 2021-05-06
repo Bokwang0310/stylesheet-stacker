a09a163a104f25715641076b2fd5ceb2c154a6a6
Bokwang0310 <chdl2353@naver.com>
2021-05-06 16:40:30 +0900

    Add home button in 404 page
     임시방편

--- src/pages/NotFoundPage.tsx
+++ src/pages/NotFoundPage.tsx
@@ -1,5 +1,12 @@
+import { Link } from 'react-router-dom';
+
 function NotFoundPage() {
-  return <h1>404 Not Found</h1>;
+  return (
+    <>
+      <h1>404 Not Found</h1>
+      <Link to="/">Go to Home</Link>
+    </>
+  );
 }
 
 export default NotFoundPage;
