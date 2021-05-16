4604e0312a8c056341fedd3e328f8b8964d15fb3
bokwang <chdl2353@naver.com>
2021-05-16 21:40:09 +0900

    Add deleting function to delete button

--- src/components/DeleteButton.tsx
+++ src/components/DeleteButton.tsx
@@ -1,4 +1,5 @@
 import { useRecoilValue } from 'recoil';
+import { useParams } from 'react-router-dom';
 
 import IconButton from '@material-ui/core/IconButton';
 
@@ -7,18 +8,25 @@ import DeleteIcon from '@material-ui/icons/Delete';
 import { modifyModeState } from 'state/modifyMode';
 import { useDispatchSection } from 'hooks/useDispatchSection';
 
+// URL 파라미터를 받아오는 리액트 라우터 훅에 쓰임
+type Param = {
+  id: string;
+};
+
 type Props = {
   id: string;
 };
 
-function DeleteButton({ id }: Props) {
+function DeleteButton({ id: SectionId }: Props) {
   const modifyMode = useRecoilValue(modifyModeState);
   const { deleteSection } = useDispatchSection();
+
+  const { id: SheetId } = useParams<Param>();
 
   return modifyMode ? (
     <IconButton
       onClick={() => {
-        // deleteSection(id);
+        deleteSection(SheetId, SectionId);
       }}
       size="small"
     >
